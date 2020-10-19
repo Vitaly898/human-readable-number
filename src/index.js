b3eb906446c21e2ca214e73ef194e35907d3a094
Vitaly898 <vetalpetrov2016@yandex.ru>
2020-10-19 16:32:04 +0300

    init: making task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (num) {
+  let numbersArr = ['','one','two','three','four','five','six','seven','eight',
+  'nine', 'ten','eleven','twelve','thirteen','fourteen','fifteen',
+  'sixteen','seventeen','eighteen','nineteen','twenty']
+  let arrOfTens= ['','','twenty','thirty',
+  'forty','fifty','sixty','seventy','eighty','ninety','one hundred']
+  let res='';
+if(num === 0){
+  return res= 'zero';
+}
+if (num<=20){
+  res = numbersArr[num]
+}
+if (num<=100 && num>20){
+  res = `${arrOfTens[Math.trunc(num/10)]} ${numbersArr[num%10]}`
+}
+if(num>100 && (num%100<20)){
+  res = `${numbersArr[Math.trunc(num/100)]} hundred ${numbersArr[num%100]}`
+}
+ if(num>100 && (num%100>=20)){
+   res = `${numbersArr[Math.trunc(num/100)]} hundred ${arrOfTens[Math.trunc((num%100)/10)]} ${numbersArr[num%10]}`
+ }
+ return res.trim()
 }
